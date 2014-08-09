7df7639e6a41b247e8936f74bf96d14256943919
Tom Forzaglia <thomas.forzaglia@gmail.com>
2014-08-08 23:04:33 -0400

    fill in chord text fields based on button clicked

--- ComposeInKey/ComposeInKey/CIKMainWindowController.m
+++ ComposeInKey/ComposeInKey/CIKMainWindowController.m
@@ -36,10 +36,15 @@
 @property (nonatomic,strong) NSMutableArray *noteArray;
 @property (nonatomic,strong) NSMutableArray *noteTextFieldArray;
 
+@property (nonatomic,strong) NSMutableArray *chordArray;
+@property (nonatomic,strong) NSMutableArray *chordTextFieldArray;
+
 /***** Private Methods *****/
 
 - (IBAction)selectKey:(id)sender;
-- (void)fillInNoteListForKey:(NSString *)key;
+- (void)fillInNoteAndChordListsForKey:(NSString *)key;
+- (void)updateNoteTextViews;
+- (void)updateChordTextViews;
 
 @end
 
@@ -48,51 +53,72 @@
 - (id)init {
     self = [super initWithWindowNibName:@"CIKMainWindow"];
     self.noteArray = [[NSMutableArray alloc] init];
+    self.chordArray = [[NSMutableArray alloc] init];
     return self;
 }
 
 - (void)windowDidLoad {
     [super windowDidLoad];
     self.noteTextFieldArray = [[NSMutableArray alloc] initWithArray:@[self.note1, self.note2, self.note3, self.note4, self.note5, self.note6, self.note7]];
+    self.chordTextFieldArray = [[NSMutableArray alloc] initWithArray:@[self.chord1, self.chord2, self.chord3, self.chord4, self.chord5, self.chord6]];
 }
 
 - (IBAction)selectKey:(id)sender {
     [self.keyName setStringValue:[sender title]];
-    [self fillInNoteListForKey:[sender title]];
+    [self fillInNoteAndChordListsForKey:[sender title]];
 }
 
-- (void)fillInNoteListForKey:(NSString *)key {
+- (void)fillInNoteAndChordListsForKey:(NSString *)key {
     if ([key isEqualToString:@"A"] || [key isEqualToString:@"F#m"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"A", @"B", @"C#", @"D", @"E", @"F#", @"G#"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"A", @"B", @"C#", @"D", @"E", @"F#", @"G#"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"A", @"Bm", @"C#m", @"D", @"E", @"F#m"]];
     } else if ([key isEqualToString:@"A#"] || [key isEqualToString:@"Gm"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"A#", @"C", @"D", @"D#", @"F", @"G", @"A"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"A#", @"C", @"D", @"D#", @"F", @"G", @"A"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"A#", @"Cm", @"Dm", @"D#", @"F", @"Gm"]];
     } else if ([key isEqualToString:@"B"] || [key isEqualToString:@"G#m"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"B", @"C#", @"D#", @"E", @"F#", @"G#", @"A#"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"B", @"C#", @"D#", @"E", @"F#", @"G#", @"A#"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"B", @"C#m", @"D#m", @"E", @"F#", @"G#m"]];
     } else if ([key isEqualToString:@"C"] || [key isEqualToString:@"Am"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"C", @"D", @"E", @"F", @"G", @"A", @"B"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"C", @"D", @"E", @"F", @"G", @"A", @"B"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"C", @"Dm", @"Em", @"F", @"G", @"Am"]];
     } else if ([key isEqualToString:@"C#"] || [key isEqualToString:@"A#m"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"C#", @"D#", @"F", @"F#", @"G#", @"A#", @"C"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"C#", @"D#", @"F", @"F#", @"G#", @"A#", @"C"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"C#", @"D#m", @"E#m", @"F#", @"G#", @"A#m"]];
     } else if ([key isEqualToString:@"D"] || [key isEqualToString:@"Bm"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"D", @"E", @"F#", @"G", @"A", @"B", @"C#"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"D", @"E", @"F#", @"G", @"A", @"B", @"C#"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"D", @"Em", @"F#m", @"G", @"A", @"Bm"]];
     } else if ([key isEqualToString:@"D#"] || [key isEqualToString:@"Cm"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"D#", @"F", @"G", @"G#", @"A#", @"C", @"D"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"D#", @"F", @"G", @"G#", @"A#", @"C", @"D"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"D#", @"Fm", @"Gm", @"G#", @"A#", @"Cm"]];
     } else if ([key isEqualToString:@"E"] || [key isEqualToString:@"C#m"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"E", @"F#", @"G#", @"A", @"B", @"C#", @"D#"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"E", @"F#", @"G#", @"A", @"B", @"C#", @"D#"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"E", @"F#m", @"G#m", @"A", @"B", @"C#m"]];
     } else if ([key isEqualToString:@"F"] || [key isEqualToString:@"Dm"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"F", @"G", @"A", @"A#", @"C", @"D", @"E"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"F", @"G", @"A", @"A#", @"C", @"D", @"E"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"F", @"Gm", @"Am", @"A#", @"C", @"Dm"]];
     } else if ([key isEqualToString:@"F#"] || [key isEqualToString:@"D#m"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"F#", @"G#", @"A#", @"B", @"C#", @"D#", @"F"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"F#", @"G#", @"A#", @"B", @"C#", @"D#", @"F"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"F#", @"G#m", @"A#m", @"B", @"C#", @"D#m"]];
     } else if ([key isEqualToString:@"G"] || [key isEqualToString:@"Em"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"G", @"A", @"B", @"C", @"D", @"E", @"F#"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"G", @"A", @"B", @"C", @"D", @"E", @"F#"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"G", @"Am", @"Bm", @"C", @"D", @"Em"]];
     } else if ([key isEqualToString:@"G#"] || [key isEqualToString:@"Fm"]) {
-        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[ @"G#", @"A#", @"C", @"C#", @"D#", @"F", @"G"]];
+        [self.noteArray replaceObjectsInRange:NSMakeRange(0, [self.noteArray count]) withObjectsFromArray:@[@"G#", @"A#", @"C", @"C#", @"D#", @"F", @"G"]];
+        [self.chordArray replaceObjectsInRange:NSMakeRange(0, [self.chordArray count]) withObjectsFromArray:@[@"G#", @"A#m", @"Cm", @"C#", @"D#", @"Fm"]];
     }
     [self updateNoteTextViews];
+    [self updateChordTextViews];
 }
 
 - (void)updateNoteTextViews {
     for (int i = 0; i < [self.noteArray count]; i++) {
         [self.noteTextFieldArray[i] setStringValue:self.noteArray[i]];
+    }
+}
+
+- (void)updateChordTextViews {
+    for (int i = 0; i < [self.chordArray count]; i++) {
+        [self.chordTextFieldArray[i] setStringValue:self.chordArray[i]];
     }
 }
 
